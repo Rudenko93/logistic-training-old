24b041498da3548308cd6232a4fc8bcd476ac7cc
Deni <dv04111993@yandex.ru>
2023-03-27 13:33:31 +0200

    20 commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ $(function() {
     let header = $("#header");
     let introH = intro.innerHeight();
     let headerH = header.innerHeight();
+    let scrollTop = $(window).scrollTop();
 
 
     /* Header class on scroll
@@ -43,6 +44,39 @@ $(function() {
             scrollTop: scrollElPos - headerH
         }, 500)
     });
+
+
+
+
+    /* ScrollSpy
+    =====================================*/
+    let windowH = $(window).height();
+    scrollSpy(scrollTop);
+
+    $(window).on("scroll", function() {
+        scrollTop = $(this).scrollTop();
+        scrollSpy(scrollTop);
+    });
+
+    function scrollSpy(scrollTop) {
+        $("[data-scrollspy]").each(function() {
+            let $this = $(this);
+            let sectionId = $this.data('scrollspy');
+            let sectionOffset = $this.offset().top;
+            sectionOffset = sectionOffset - (windowH * 0.33333);
+
+            if(scrollTop >= sectionOffset) {
+                $('#nav [data-scroll]').removeClass('active');
+                $('#nav [data-scroll="' + sectionId + '"]').addClass('active');
+            }
+
+            if(scrollTop == 0) {
+                $('#nav [data-scroll]').removeClass('active');
+            }
+        });
+    }
+
+
 
 
 
